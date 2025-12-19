068a4aea7d34373f936ff24fa13389f4a4c91ccc
Bala Sudalaimuthu <bala@Balas-MacBook-Air.local>
2025-12-19 17:55:54 +0530

    Changed url

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:3000/api/:path*',
+        destination: 'https://ims-backend-pied.vercel.app/api/:path*',
       },
     ];
   },
